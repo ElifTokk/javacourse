6e76aed6719f8f8949acbbe4735b4dd3224d4e50
ElifTokk <elifmarttok@gmail.com>
2024-04-16 16:02:54 +0300

    odev8

--- javacourse/src/main/java/com/test/javacourse/MarketPlace/controller/OrderController.java
+++ javacourse/src/main/java/com/test/javacourse/MarketPlace/controller/OrderController.java
@@ -5,12 +5,11 @@ import com.test.javacourse.MarketPlace.exceptionHandling.BusinessException;
 import com.test.javacourse.MarketPlace.service.OrderService;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
 @RequestMapping("/order")
 public class OrderController {
 
-    private final OrderService orderService;
+private final OrderService orderService;
 
     public OrderController(OrderService orderService) {
         this.orderService = orderService;
